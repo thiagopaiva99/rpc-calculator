1013bb33c2c511166ae61654e999ee7bec9dc95e
Thiago Galvani <thiagopaiva99@gmail.com>
2018-09-11 21:17:30 -0300

    Refactoring main file

--- calculadora_simples.c
+++ calculadora_simples.c
@@ -2,22 +2,33 @@
 #include <stdlib.h>
 #include "calculadora.h"
 
-int main(int argc, char *argv[])
-{
- CLIENT * clnt;
- int x, y;
- if(argc!=4)
- {
-	fprintf(stderr,"uso: %s host num1 num\n",argv[0]);
-	exit(0);
- } 
- clnt = clnt_create(argv[1], CALCULADORA,CALCULADORA_VERSAO,"udp");
- operando ops;
- ops.x = atoi(argv[2]);
- ops.y = atoi(argv[3]);
- printf("%d + %d = %d\n", ops.x, ops.y, *somar_1(&ops, clnt));
- printf("%d - %d = %d\n", ops.x, ops.y, *subtrair_1(&ops, clnt));
- printf("%d * %d = %d\n", ops.x, ops.y, *multiplicar_1(&ops, clnt));
- printf("%d / %d = %d\n", ops.x, ops.y, *dividir_1(&ops, clnt));
- return(0);
+/* Check if args passed are OK */
+void checkArgs(int argc, char *argv[]) {
+ 	if(argc != 4)
+	{
+		fprintf(stderr,"How to use: %s <HOST> <FIRST_VALUE> <SECOND_VALUE>\n", argv[0]);
+		exit(0);
+ 	} 
+}
+
+/* Printing the final results */
+void printResults(operando ops, CLIENT * clnt) {
+	printf("%d + %d = %d\n", ops.x, ops.y, *somar_1(&ops, clnt));
+ 	printf("%d - %d = %d\n", ops.x, ops.y, *subtrair_1(&ops, clnt));
+ 	printf("%d * %d = %d\n", ops.x, ops.y, *multiplicar_1(&ops, clnt));
+ 	printf("%d / %d = %d\n", ops.x, ops.y, *dividir_1(&ops, clnt));
+}
+
+void main(int argc, char *argv[]) {
+ 	checkArgs(argc, argv); 
+ 	
+	int x, y;
+
+ 	CLIENT * clnt = clnt_create(argv[1], CALCULADORA, CALCULADORA_VERSAO, "udp"); 	
+
+	operando ops;
+ 	ops.x = atoi(argv[2]);
+ 	ops.y = atoi(argv[3]);
+
+ 	printResults(ops, clnt);
 }
